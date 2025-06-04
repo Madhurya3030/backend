73041f1b9ba325b038e123e77adb39ae28187bb1
Gunupudi Durga Madhurya <111988941+Madhurya3030@users.noreply.github.com>
2025-06-04 22:34:44 +0530

    commited

--- utils/mailer.js
+++ utils/mailer.js
@@ -12,20 +12,22 @@ const transporter = nodemailer.createTransport({
 async function sendVerificationEmail(email, token) {
   try {
     const url = `https://homecare-backend-ke7r.onrender.com/api/auth/verify?token=${token}`;
-
-
-
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: email,
       subject: 'Verify your email',
       html: `<p>Click the link to verify your email: <a href="${url}">${url}</a></p>`
     };
-    await transporter.sendMail(mailOptions);
+
+    console.log(`Sending email to ${email}...`);
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully:', info.response);
+
   } catch (error) {
-    console.error('Error sending verification email:', error);
+    console.error('❌ Error sending verification email:', error.message);
     throw error;
   }
 }
 
 module.exports = sendVerificationEmail;
+

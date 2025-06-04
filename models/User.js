36b7b672bef1d714d5cc6f39875c1b7358a40d50
Gunupudi Durga Madhurya <111988941+Madhurya3030@users.noreply.github.com>
2025-06-04 22:18:33 +0530

    added emial route

--- models/User.js
+++ models/User.js
@@ -3,14 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   isVerified: { type: Boolean, default: false },
   verifyToken: { type: String }
-=======
->>>>>>> f48583d2789b1675fe8aa71e452ef9139f13f302
-=======
->>>>>>> f48583d2789b1675fe8aa71e452ef9139f13f302
+
 });
 
 module.exports = mongoose.model('User', userSchema);

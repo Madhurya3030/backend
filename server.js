ec16d712ad80ad0d048dc669467e45d9594671f7
Gunupudi Durga Madhurya <111988941+Madhurya3030@users.noreply.github.com>
2025-06-05 10:49:34 +0530

    commit

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
+
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
